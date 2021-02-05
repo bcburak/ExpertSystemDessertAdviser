7433b9afc970717640b3425a6233551b5b551038
Burak Çelik <burak.celik@kron.com.tr>
2021-02-05 21:26:07 +0300

    adviser form added

---
+++ ExpertSystemDessertAdviser/AdviserForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace ExpertSystemDessertAdviser
+{
+    partial class AdviserForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(259, 114);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "advise1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(345, 129);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(271, 111);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "advise2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(645, 129);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(303, 111);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "advise3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(62, 273);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(898, 36);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Sizin için belirlediğimiz öneriler bunlar.Hangisini seçmek istersiniz?";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(444, 359);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(126, 49);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Yenile ???";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // AdviserForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1088, 536);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "AdviserForm";
+            this.Text = "Öneri Seçimi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+    }
+}
--- ExpertSystemDessertAdviser/Approvement.Designer.cs
+++ ExpertSystemDessertAdviser/Approvement.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace ExpertSystemDessertAdviser
 {
-    partial class Form1
+    partial class Approvement
     {
         /// <summary>
         /// Required designer variable.
@@ -29,39 +29,38 @@ namespace ExpertSystemDessertAdviser
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(267, 228);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(193, 54);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Evet Lütfen";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(117, 71);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(54, 88);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(578, 46);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Yemek yanında tatlı ister misin?";
+            this.label1.Size = new System.Drawing.Size(952, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tebrikler seçiminizi tamamladınız şimdi ödeme sayfasına geçebilirsiniz..";
             // 
-            // Form1
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(455, 203);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Approvement
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(1042, 388);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.label1);
+            this.Name = "Approvement";
+            this.Text = "Approvement";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -69,8 +68,7 @@ namespace ExpertSystemDessertAdviser
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
-
